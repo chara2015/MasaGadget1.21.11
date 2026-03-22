a664277c54d0b9b4a8a3399f4bc6c4168ec69ff3
POPBO <POPBOB2011@outlook.com>
2026-03-23 01:25:47 +0800

    更改README和版本号系统

--- src/main/java/com/plusls/MasaGadget/ModInfo.java
+++ src/main/java/com/plusls/MasaGadget/ModInfo.java
@@ -2,8 +2,9 @@ package com.plusls.MasaGadget;
 
 /**
  * Central mod metadata. All version/name/author info lives here.
- * build.gradle reads MOD_VERSION from this file for the output jar name.
+ * build.gradle reads constants from this file for the output jar name and fabric.mod.json.
  */
+@SuppressWarnings("unused")
 public final class ModInfo {
     private ModInfo() {}
 
@@ -11,7 +12,7 @@ public final class ModInfo {
     public static final String MOD_NAME        = "MasaGadget";
     public static final String MOD_VERSION     = "v0.2beta";
     public static final String MOD_DESCRIPTION = "Added some features to the Masa collection of mods.";
-    public static final String MOD_AUTHORS     = "plusls, Hendrix-Shen, chara201x, claude4.6-opus";
+    public static final String MOD_AUTHORS     = "plusls, Hendrix-Shen, chara201x, claude-opus";
     public static final String MOD_LICENSE     = "LGPL-3.0";
     public static final String MOD_HOMEPAGE    = "https://blog.plusls.com/";
     public static final String MOD_SOURCES     = "https://github.com/plusls/MasaGadget";
